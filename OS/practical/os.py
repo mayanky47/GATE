6ba0c7bda4af24ec6ec4dfa558676d6e5f5c8588
Mayank <mayanky500@gmail.com>
2024-07-05 18:24:11 +0530

    dbms stuff

---
+++ OS/practical/os.py
@@ -0,0 +1,18 @@
+# A process a program in execution
+
+#let assume I have some program like
+
+def sum(a,b):
+	return a+b
+def multiply(a,b):
+	return a*b
+def subtract(a,b):
+	return a-b
+
+#So now how to run these program
+
+# First we need them in memory
+
+	#To do that we need a schedular
+	#A queue in memory to keep those program
+
